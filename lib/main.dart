0fa8a5b3f12b9f6767ff42fe78b6ebb7f859ce33
djBrinee <deivyjunior12@gmail.com>
2023-05-29 09:55:05 -0400

    Adding const keywords where's necessary

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:class3/quiz_brain.dart';
 import 'package:flutter/material.dart';
 import 'package:restart_app/restart_app.dart';
 
-QuizBrain quiz = new QuizBrain();
+QuizBrain quiz = QuizBrain();
 List<Widget> scoreKeeper = [];
 int goodAnswers = 0;
 
-void main() => runApp(Quizzler());
+void main() => runApp(const Quizzler());
 
 class Quizzler extends StatelessWidget {
   const Quizzler({super.key});
@@ -57,7 +57,7 @@ class _QuizPageState extends State<QuizPage> {
               child: Text(
                 quiz.getQuestionText,
                 textAlign: TextAlign.center,
-                style: TextStyle(
+                style: const TextStyle(
                   fontSize: 25.0,
                   color: Colors.white,
                 ),
@@ -154,21 +154,21 @@ class _QuizPageState extends State<QuizPage> {
       barrierDismissible: false,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text("You've finished the game",
+          title: const Text("You've finished the game",
               style: TextStyle(fontSize: 30, color: Color.fromARGB(255, 4, 31, 154))),
           content: SingleChildScrollView(
             child: ListBody(
               children: <Widget>[
                 Text(
-                  'You had a total of ${correctAnswers} over ${scoreKeeper.length} total answers. You can play again by pressing restart.',
-                  style: TextStyle(fontSize: 20),
+                  'You had a total of $correctAnswers over ${scoreKeeper.length} total answers. You can play again by pressing restart.',
+                  style: const TextStyle(fontSize: 20),
                 ),
               ],
             ),
           ),
           actions: <Widget>[
             TextButton(
-              child: Text('Reestart',
+              child: const Text('Reestart',
                   style: TextStyle(
                     fontSize: 24,
                   )),
@@ -187,9 +187,3 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
